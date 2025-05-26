4a11cf1fc79d70d673a0685af4fbd580bd07ab42
sonicheo <juan.a.gomez.villanueva@gmail.com>
2025-05-26 15:24:46 -0700

    added a couple of things in fundamentals

--- 01-fundamentals/mine/src/BookList.js
+++ 01-fundamentals/mine/src/BookList.js
@@ -2,37 +2,72 @@ import React from 'react'
  
 import './index.css';
 
-const BookList = () => {
-  return (
-    <>
-        <section className="booklist">
-          <Book/>
-          <Book/>
-          <Book/>
-          <Book/>
-        </section>
-    </>
+const books =[
+  {
+    author :"Evan Stanley",
+    title : "Sonic The Hedgehog (2018-) #46",
+    img : "https://m.media-amazon.com/images/I/51HkeecClwL._SY445_SX342_PQ26_.jpg",
+    id: 1
+  },
+  {
+    author :"Evan Stanley",
+    title : "Sonic the Hedgehog: Imposter Syndrome #1 (of 4)",
+    img : "https://m.media-amazon.com/images/I/91tV6oUj89L._SY466_.jpg",
+    id: 2,
+  }
+]
+
+
+const EventEx = () =>{
+  const handleFormInput = (e) =>{
+    console.log(e);
+  }
+  const handleButtonClick = () =>{
+    alert("test");
+  }
+
+  return(
+    <section>
+      <form>
+        <h2>Form</h2>
+        <input type="text" name="example" onChange={handleFormInput}  style={{margin: "1rem 0"}} />
+      </form>
+      <button onClick={handleButtonClick} >click me</button>
+    </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  const {img, title, author} = props;
   return(
     <article className="book">
-      <Image/>
-      <Title/>
-      <Author/>
+      <img src={img} alt=""/>
+      <h4>{author}</h4>
+      <h2>{title}</h2>
     </article>
   )
 }
 
-const Image = () => {
-  return <img src="https://m.media-amazon.com/images/I/91bqp45rHoL._SY466_.jpg" alt=""/>
+const books_map = books.map((book)=>{
+  return(
+    <Book {...book} key={book.id}/>
+  )
+});
+ 
+
+const BookList = () => {
+  return (
+    <>
+        <section className="booklist">
+          <EventEx/>
+          {books_map}
+        </section>;
+    </>
+  )
 }
 
-const Title = () => {
-  return <h2>Sonic The Hedgehog (2018-) #46</h2>
-}
 
-const Author = () => <h4>Evan Stanley</h4>
+
+
 
 export default BookList
